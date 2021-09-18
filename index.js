f78f8d5e884704bcc7b8bb08771c5f799ae8cf74
Mahgam Elmi <52955585+mahgam@users.noreply.github.com>
2021-09-18 16:49:13 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+var count = 0
+function increment() {
+    count = count + 1
+    document.getElementById("count-elid").innerText = count
+    console.log(document.getElementById("count-elid").innerText)
+}
+console.log("help me not please")
+function save() {
+    console.log("I hate you")
+    console.log(count)
+
+}
+
