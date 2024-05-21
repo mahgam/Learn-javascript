0428e56752e2e0a86d89e2ecc9df60601def3802
Mahgam Elmi <52955585+mahgam@users.noreply.github.com>
2024-05-21 12:59:28 -0700

    Update fizbuzz.js

--- little games/fizbuzz.js
+++ little games/fizbuzz.js
@@ -1,1 +1,18 @@
+let output = [];
+function fizzBuzz() {
+  for (i = 0; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      output.push("fizbuzz");
+    } else if (i % 3 == 0) {
+      output.push("fizz");
+    } else if (i % 5 == 0) {
+      output.push("buzz");
+    } else {
+      output.push(i);
+    }
+  }
+  console.log(output);
+}
+
+fizzBuzz();
 
